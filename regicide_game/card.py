03084a1f95703d06107fef7644b6a15190616789
Patryk Szczepanski <patryk.w.panski@gmail.com>
2023-10-27 14:29:06 +0100

    Added create_tavern_deck function and unit tests.

--- regicide_game/card.py
+++ regicide_game/card.py
@@ -37,7 +37,7 @@ class Card:
     suit: Suit
 
     def __repr__(self):
-        return self.name.value + self.suit.value
+        return str(self.name.value) + self.suit.value
 
 
 def create_castle_deck() -> List[Card]:
@@ -62,3 +62,57 @@ def create_castle_deck() -> List[Card]:
     random.shuffle(kings)
 
     return jacks + queens + kings
+
+
+def create_tavern_deck(num_players) -> List[Card]:
+    """
+    This creates a shuffled deck with all the cards numbered 2 to 10 with the 4 Animal Companions (Ace) and Jesters.
+    The number of Jesters depends on the number of players.
+    """
+    if num_players not in [1, 2, 3, 4]:
+        raise ValueError(
+            f"'{num_players}' is not a valid number of players. Valid number of players are '1', '2', '3', and '4'."
+        )
+
+    suits = list(Suit)
+    suits.remove(Suit.JESTER)
+
+    tavern_deck = []
+
+    # add 4 copies of each card (2-10 and Aces) with different suits
+    for name in CardName:
+        for suit in suits:
+            if name in [
+                CardName.TWO,
+                CardName.THREE,
+                CardName.FOUR,
+                CardName.FIVE,
+                CardName.SIX,
+                CardName.SEVEN,
+                CardName.EIGHT,
+                CardName.NINE,
+                CardName.TEN,
+            ]:
+                tavern_deck.append(
+                    Card(name=name, power=name.value, health=name.value, suit=suit)
+                )
+            if name == CardName.ACE:
+                tavern_deck.append(Card(name=name, power=1, health=1, suit=suit))
+
+    # add Jesters based on the number of players
+    if num_players == 3:
+        tavern_deck.append(
+            Card(name=CardName.JESTER, power=0, health=0, suit=Suit.JESTER)
+        )
+    elif num_players == 4:
+        tavern_deck.append(
+            Card(name=CardName.JESTER, power=0, health=0, suit=Suit.JESTER)
+        )
+        tavern_deck.append(
+            Card(name=CardName.JESTER, power=0, health=0, suit=Suit.JESTER)
+        )
+
+    # shuffle the deck
+    random.shuffle(tavern_deck)
+
+    return tavern_deck
--- tests/test_card.py
+++ tests/test_card.py
@@ -1,6 +1,6 @@
 import unittest
 
-from regicide_game.card import CardName, Suit, create_castle_deck
+from regicide_game.card import CardName, Suit, create_castle_deck, create_tavern_deck
 
 
 class TestCreateCastleDeck(unittest.TestCase):
@@ -31,6 +31,36 @@ class TestCreateCastleDeck(unittest.TestCase):
                 self.assertEqual(castle_deck[i].name, CardName.QUEEN)
             else:
                 self.assertEqual(castle_deck[i].name, CardName.KING)
+
+
+class TestCreateTavernDeck(unittest.TestCase):
+    def test_there_are_all_expected_card_names_4each_with_unique_suits(self):
+        tavern_deck = create_tavern_deck(3)
+
+        card_names = list(CardName)
+        card_names = [
+            e
+            for e in card_names
+            if e not in (CardName.JACK, CardName.QUEEN, CardName.KING, CardName.JESTER)
+        ]
+
+        # dictionary holding a CardName to list of suits mapping
+        unique_cards = {}
+
+        for name in card_names:
+            unique_cards[name] = []
+
+        # iterate over the tavern deck and add the card:suit pairing to the dictionary
+        for card in tavern_deck:
+            if (
+                card.name != CardName.JESTER
+                and card.suit not in unique_cards[card.name]
+            ):
+                unique_cards[card.name].append(card.suit)
+
+        # validate each card in the dictionary is assigned a list of length 4
+        for card in unique_cards:
+            self.assertEqual(len(unique_cards[card]), 4)
 
 
 if __name__ == "__main__":
